23c4393406da8c2ea1ae811d2ce3914d395abb6c
darinnn <georgiev.darin@gmail.com>
2015-09-05 00:17:01 +0300

    Add Edit Team and Save As functionality

--- public/js/controllers/MainController.js
+++ public/js/controllers/MainController.js
@@ -2,6 +2,8 @@
 app.controller('MainController', ['$scope', 'teamService', 'notify', function ($scope, teamService, notify) {
     var self = this;
     self.isCreateNewTeamVisible = false;
+    self.isEditTeamVisible = false;
+    self.isSaveAsTeamVisible = false;
 
     self.getAllTeams = function () {
         teamService.getAllTeams()
@@ -21,6 +23,7 @@ app.controller('MainController', ['$scope', 'teamService', 'notify', function ($
             .success(function (data) {
                 self.selectedTeam = data;
                 self.isCreateNewTeamVisible = false;
+                self.isEditTeamVisible = false;
                 self.allTeams.push(data);
             })
             .error(function (data) {
@@ -60,13 +63,15 @@ app.controller('MainController', ['$scope', 'teamService', 'notify', function ($
         teamService.updateTeam(self.selectedTeam)
             .success(function (data) {
                 //self.selectedTeam = data;
-                //notify({message: 'Team: ' + data.teamName + '\n Updated successful', classes: 'alert-danger', position: 'left'});
+                notify({message: 'Team: ' + data.teamName + '\n Updated successful', classes: 'noty', position: 'center'});
+                self.isEditTeamVisible = false;
             })
             .error(function (data) {
                 console.log('Error: ' + data);
             });
     };
 
+    //Create New Team
     self.onCreateNewTeamClick = function () {
         self.isCreateNewTeamVisible = true;
         self.newTeam = JSON.parse(JSON.stringify(self.allTeams[0]));
@@ -77,6 +82,34 @@ app.controller('MainController', ['$scope', 'teamService', 'notify', function ($
         self.isCreateNewTeamVisible = false;
     };
 
+    //Edit Team
+    self.onEditTeamClick = function () {
+        self.isEditTeamVisible = true;
+    };
+
+    //Save As Tea,
+    self.onSaveAsTeamClick = function () {
+        self.isSaveAsTeamVisible = true;
+    };
+
+    self.onEditTeamCancelClick = function () {
+        self.isEditTeamVisible = false;
+        self.isSaveAsTeamVisible = false;
+    };
+
+    $scope.demoMessageTemplate = function(){
+
+        var messageTemplate = '<div class="alert alert-dismissible alert-danger"><button type="button" class="close" data-dismiss="alert">×</button><strong>Oh snap!</strong> <a href="#" class="alert-link">Change a few things up</a> and try submitting again.</div>';
+
+        notify({
+            messageTemplate: messageTemplate,
+            classes: 'alert-danger',
+            scope:$scope,
+            //\templateUrl: $scope.template,
+            position: 'left'
+        });
+
+    };
     ////Hack for resizing
     //self.setDragableAreaWidth = function () {
     //    $('.draggableArea').width($('.imgField').width() - $('.player').width());
@@ -88,41 +121,42 @@ app.controller('MainController', ['$scope', 'teamService', 'notify', function ($
     //}();
 
     //ng-sortable
-    //$scope.dragControlListeners = {
-    //    accept: function (sourceItemHandleScope, destSortableScope) {
-    //        console.log("accept");
-    //        return boolean
-    //    },
-    //    //override to determine drag is allowed or not. default is true.
-    //    itemMoved: function (event) {
-    //        console.log("move");
-    //    },
-    //    //Do what you want},
-    //    orderChanged: function (event) {
-    //        console.log("oredr");
-    //    },
-    //    //Do what you want},
-    //    containment: '#board'//optional param.
-    //};
-    //
+
+    $scope.dragControlListeners = {
+        accept: function (sourceItemHandleScope, destSortableScope) {
+            console.log("accept");
+            return boolean
+        },
+        //override to determine drag is allowed or not. default is true.
+        itemMoved: function (event) {
+            console.log("move");
+        },
+        //Do what you want},
+        orderChanged: function (event) {
+            console.log("oredr");
+        },
+        //Do what you want},
+        containment: '#board'//optional param.
+    };
+
     //$scope.sortableOptions = {
     //
     //}
-    //$scope.dropControlListeners = {
-    //    accept: function (sourceItemHandleScope, destSortableScope) {
-    //        console.log("accept");
-    //        return boolean
-    //    },
-    //    //override to determine drag is allowed or not. default is true.
-    //    itemMoved: function (event) {
-    //        console.log("move");
-    //    },
-    //    //Do what you want},
-    //    orderChanged: function (event) {
-    //        console.log("oredr");
-    //    },
-    //    //Do what you want},
-    //    containment: '#board'//optional param.
-    //}
+    $scope.dropControlListeners = {
+        accept: function (sourceItemHandleScope, destSortableScope) {
+            console.log("accept");
+            return boolean
+        },
+        //override to determine drag is allowed or not. default is true.
+        itemMoved: function (event) {
+            console.log("move");
+        },
+        //Do what you want},
+        orderChanged: function (event) {
+            console.log("oredr");
+        },
+        //Do what you want},
+        containment: '#board'//optional param.
+    }
 
 }]);
