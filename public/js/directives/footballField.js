7e96e160fce4231cace83223f8dcc6c6baa9aff6
darinnn <georgiev.darin@gmail.com>
2016-03-30 01:56:33 +0300

    Add Sass

--- public/js/directives/footballField.js
+++ public/js/directives/footballField.js
@@ -6,15 +6,5 @@ app.directive('footballField',function(){
         restrict: 'E',
         templateUrl: '../../templates/football-field.html',
         replace: true,
-        link: function (scope, element, attr) {
-            setTimeout(function(){
-                $('.draggableArea').width($('.imgField').width() - $('.playerWrapper').width());
-                $('.draggableArea').css({left: $('.imgField').position().left});
-                $(window).on('resize', function () {
-                    $('.draggableArea').width($('.imgField').width() - $('.playerWrapper').width());
-                    $('.draggableArea').css({left: $('.imgField').position().left});
-                });
-            },100)
-        }
     }
 });
