7f464102c91e9bd18b0aebcd6a04a7fcb47a1aac
darinnn <georgiev.darin@gmail.com>
2016-03-31 00:32:27 +0300

    Add routes

--- public/js/app.js
+++ public/js/app.js
@@ -4,9 +4,21 @@ var app = angular.module('app', ['cgNotify', 'ui.sortable', 'ngRoute'])
 .config(['$routeProvider', '$locationProvider',
     function($routeProvider, $locationProvider) {
         'use strict';
-        $routeProvider.
-            when('/', {
+        $routeProvider
+            .when('/', {
                 reloadOnSearch:false
+            })
+            .when('/register', {
+                templateUrl: 'templates/register-form.html',
+            })
+            .when('/edit-team', {
+                templateUrl: 'templates/edit-team.html',
+            })
+            .when('/create-new-team', {
+                templateUrl: 'templates/new-team.html',
+            })
+            .when('/save-as-team', {
+                templateUrl: 'templates/edit-team.html',
             });
         $locationProvider.html5Mode({
             enabled: true,
